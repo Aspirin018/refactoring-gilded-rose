06ca29e01238ac0acdc26a08ccbcba120d8af5c8
Aspirin018 <438238118@qq.com>
2020-03-01 22:51:16 +0800

    抽取方法：将计算非特殊商品的逻辑单独抽成方法

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,5 +1,7 @@
 package com.gildedrose;
 
+import java.util.Arrays;
+
 class GildedRose {
     Item[] items;
 
@@ -9,54 +11,53 @@ class GildedRose {
 
     public void update_quality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
+            if (Arrays.asList("Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros").contains(items[i])) {
+                updateDefaultItemQuality(items[i]);
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
+                if (items[i].name.equals("Aged Brie") || items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                     if (items[i].quality < 50) {
                         items[i].quality = items[i].quality + 1;
+
+                        if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                            if (items[i].sellIn < 11) {
+                                if (items[i].quality < 50) {
+                                    items[i].quality = items[i].quality + 1;
+                                }
+                            }
+
+                            if (items[i].sellIn < 6) {
+                                if (items[i].quality < 50) {
+                                    items[i].quality = items[i].quality + 1;
+                                }
+                            }
+                        }
+                    }
+                }
+                if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                    items[i].sellIn = items[i].sellIn - 1;
+                }
+                if (items[i].sellIn < 0) {
+                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                        items[i].quality = items[i].quality - items[i].quality;
+                    }
+                    if (items[i].name.equals("Aged Brie")) {
+                        if (items[i].quality < 50) {
+                            items[i].quality = items[i].quality + 1;
+                        }
                     }
                 }
             }
+        }
+    }
+
+
+    private void updateDefaultItemQuality(Item item) {
+        if (item.quality > 0) {
+            item.quality--;
+        }
+        item.sellIn--;
+        if (item.sellIn < 0 && item.quality > 0) {
+            item.quality--;
         }
     }
 }
