a515d917353f4ec5f4e9f6d986d0bf1647c9d395
Aspirin018 <438238118@qq.com>
2020-03-01 23:46:21 +0800

    重构AgesBrieUpdater中的计算逻辑

--- src/main/java/com/gildedrose/AgedBrieUpdater.java
+++ src/main/java/com/gildedrose/AgedBrieUpdater.java
@@ -6,11 +6,10 @@ public class AgedBrieUpdater implements ItemUpdater {
     private static final int CRITICAL_SELL_IN = 0;
     @Override
     public void update(Item item) {
-        item.sellIn--;
         if (item.quality < CRITICAL_QUALITY) {
             item.quality++;
         }
-        if (item.sellIn < CRITICAL_SELL_IN && item.quality < CRITICAL_QUALITY) {
+        if (--item.sellIn < CRITICAL_SELL_IN && item.quality < CRITICAL_QUALITY) {
             item.quality++;
         }
     }
