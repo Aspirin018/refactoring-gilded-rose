f5d05810e9bb96f313e614fb4304031554ffbfae
Aspirin018 <438238118@qq.com>
2020-03-01 23:25:17 +0800

    在GildedRose中使用updater计算。

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,23 +1,31 @@
 package com.gildedrose;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 class GildedRose {
     Item[] items;
+
+    private static final Map<String, ItemUpdater> specialUpdateMapper = new HashMap<>(3);
 
     public GildedRose(Item[] items) {
         this.items = items;
     }
 
+    static {
+        specialUpdateMapper.put("Aged Brie", new AgedBrieUpdater());
+        specialUpdateMapper.put("Backstage passes to a TAFKAL80ETC concert", new BackstageUpdater());
+        specialUpdateMapper.put("Sulfuras, Hand of Ragnaros", new SulfurasUpdater());
+    }
+
     public void update_quality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie") && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert") && !items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                new DefaultItemUpdater().update(items[i]);
-            } else if (items[i].name.equals("Aged Brie")) {
-                new AgedBrieUpdater().update(items[i]);
-            } else if(items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")){
-                new BackstageUpdater().update(items[i]);
+        Arrays.stream(items).forEach(item -> {
+            if (specialUpdateMapper.keySet().contains(item.name)) {
+                specialUpdateMapper.get(item.name).update(item);
             } else {
-                new SulfurasUpdater().update(items[i]);
+                new DefaultItemUpdater().update(item);
             }
-        }
+        });
     }
 }
