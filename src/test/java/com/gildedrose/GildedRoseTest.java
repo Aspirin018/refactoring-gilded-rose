8853be7f975ca5b4ed7d79a62333c05eab10477b
Aspirin018 <438238118@qq.com>
2020-03-01 21:53:02 +0800

    创建测试用例1：非特殊商品且价值大于零时，价值和保质期减一。

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,5 +1,19 @@
 package com.gildedrose;
 
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+
 public class GildedRoseTest {
+
+    @Test
+    public void should_reduced_by_1_when_is_not_special_item_and_quality_more_than_zero() {
+        Item[] items = new Item[] { new Item("milk", 1, 5) };
+        GildedRose app = new GildedRose(items);
+        app.update_quality();
+        assertThat(app.items[0].quality, is(4));
+        assertThat(app.items[0].sell_in, is(0));
+    }
 }
