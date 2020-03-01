80ad9bcc2d273919ff2fda00ff17c5cd9f210d89
Aspirin018 <438238118@qq.com>
2020-03-01 23:04:12 +0800

    抽取方法：将计算Backstage的逻辑单独抽成方法

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -13,37 +13,39 @@ class GildedRose {
         for (int i = 0; i < items.length; i++) {
             if (!items[i].name.equals("Aged Brie") && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert") && !items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                 updateDefaultItemQuality(items[i]);
-            } else if(items[i].name.equals("Aged Brie")){
+            } else if (items[i].name.equals("Aged Brie")) {
                 updateAgedBrieQuality(items[i]);
+            } else if(items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")){
+                updateBackstageQuality(items[i]);
             } else {
-                if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-
-                        if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                            if (items[i].sellIn < 11) {
-                                if (items[i].quality < 50) {
-                                    items[i].quality = items[i].quality + 1;
-                                }
-                            }
-
-                            if (items[i].sellIn < 6) {
-                                if (items[i].quality < 50) {
-                                    items[i].quality = items[i].quality + 1;
-                                }
-                            }
-                        }
-                    }
-                }
                 if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                     items[i].sellIn = items[i].sellIn - 1;
                 }
-                if (items[i].sellIn < 0) {
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                }
             }
+        }
+    }
+
+    private void updateBackstageQuality(Item item) {
+        if (item.quality < 50) {
+            item.quality++;
+        }
+
+        if (item.sellIn < 11) {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
+            }
+        }
+
+        if (item.sellIn < 6) {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
+            }
+        }
+
+        item.sellIn--;
+
+        if (item.sellIn < 0) {
+            item.quality = 0;
         }
     }
 
