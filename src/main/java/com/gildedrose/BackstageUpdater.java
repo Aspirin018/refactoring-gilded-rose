d562a30ec5a0fbe54bb71a6db1de24bd2dad6140
Aspirin018 <438238118@qq.com>
2020-03-01 23:43:36 +0800

    重构BackstageUpdater中的计算逻辑

--- src/main/java/com/gildedrose/BackstageUpdater.java
+++ src/main/java/com/gildedrose/BackstageUpdater.java
@@ -8,22 +8,21 @@ public class BackstageUpdater implements ItemUpdater {
     private static final int SELL_IN_QUALITY = 0;
     @Override
     public void update(Item item) {
-        if (item.quality < CRITICAL_QUALITY) {
-            item.quality++;
-        }
 
-        if (item.sellIn < SELL_IN_UPPER_LIMIT && item.quality < CRITICAL_QUALITY) {
-            item.quality++;
-        }
-
-        if (item.sellIn < SELL_IN_LOWER_LIMIT && item.quality < CRITICAL_QUALITY) {
-            item.quality++;
-        }
-
-        item.sellIn--;
-
-        if (item.sellIn < SELL_IN_QUALITY) {
+        if (--item.sellIn < SELL_IN_QUALITY) {
             item.quality = 0;
+        } else {
+            if (item.quality < CRITICAL_QUALITY) {
+                item.quality++;
+            }
+
+            if (item.sellIn < SELL_IN_UPPER_LIMIT && item.quality < CRITICAL_QUALITY) {
+                item.quality++;
+            }
+
+            if (item.sellIn < SELL_IN_LOWER_LIMIT && item.quality < CRITICAL_QUALITY) {
+                item.quality++;
+            }
         }
     }
 }
